070b5cfd97278a890097d8b5560bce5e056d8b41
Ryan Doty <ryandoty@Ryans-MacBook-Pro.local>
2019-08-08 13:49:11 -0400

    updates to col sizing

--- client/src/pages/WorkoutProgramPick.js
+++ client/src/pages/WorkoutProgramPick.js
@@ -9,13 +9,44 @@ class WorkoutProgramPick extends Component {
 
 
                 <Jumbotron>
-                <h1> WORKOUT APP: PROGRAM PICKER!</h1>
+                    <h1> WORKOUT APP: PROGRAM PICKER!</h1>
                 </Jumbotron>
 
+                <Container>
+                   <Row>
+                       <h2> row 1</h2>
+                       <Col size="md-6">
+                       <h2> col 1</h2>
+                       </Col>
+                       <Col size="md-6">
+                       <h2> col 2</h2>
+                       </Col>
+                   </Row>
+               </Container>
 
-            </Container>
-        );
-    }
+
+               <Container>
+                    <h2> row 2</h2>
+                    <Row>
+                        <Col size="md-6">
+                        <h2> col 1</h2>
+                        </Col>
+                        <Col size="md-6">
+                        <h2> col 2</h2>
+                        </Col>
+                    </Row>
+               </Container>
+           </Container>
+       )
+   }
+
+                
+
+
+
+
 }
 
 export default WorkoutProgramPick;
+
+
