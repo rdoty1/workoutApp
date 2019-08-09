533d1fa716a98dab5c7d5a623bd112b82476b387
Ryan Doty <ryandoty@Ryans-MacBook-Pro.local>
2019-08-08 20:45:49 -0400

    css updates

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -51,9 +51,6 @@ class HomePage extends Component {
         event.preventDefault();
         console.log(this.state.firstName);
          const user = {
-
-
-
           firstName:this.state.firstName,
           lastName: this.state.lastName,
           email: this.state.email,
@@ -90,69 +87,133 @@ class HomePage extends Component {
                 </h1>
           
                 <Container>
+                  <div className="userInfo">
                 <form>
-
+                <div class="container">
+                <br></br>
+                <div class="row">
+                <div class="col-lg-6">
                   <label>
                     First Name:
                   </label>
+                  </div>
+                  <div class="col-lg-6">
                   <input className="boxes" type="text" name="firstName"  value={this.state.firstName} onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
 
+                <div class="row">
+                <div class="col-lg-6">
                   <label>
                     Last Name:
                   </label>
-                  <input className="boxes" type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className="" type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
 
+                <div class="row">
+                <div class="col-lg-6">
                   <label>
                   Email:
                   </label>
-                  <input className="boxes" type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className="" type="text" name="email" value={this.state.email} onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
 
+                  <div class="row">
+                <div class="col-lg-6">
                   <label>
                   Password:
                   </label>
-                  <input className="boxes" type="text" name="password" value={this.state.password} onChange={this.handleChange} />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className="" type="text" name="password" value={this.state.password} onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
 
+                  <div class="row">
+                <div class="col-lg-6">
                   <label>
                   Your Age:
                   </label>
-                  <input className="boxes" type="text" name="age" value={this.state.age} onChange={this.handleChange} />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className="" type="text" name="age" value={this.state.age} onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
 
+                  <div class="row">
+                <div class="col-lg-6">
                   <label>
                   Your Weight (lbs):
                   </label>
-                  <input className="boxes" type="text" name="weight" value={this.state.weight} onChange={this.handleChange}  />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className='' type="text" name="weight" value={this.state.weight} onChange={this.handleChange}  />
+                  </div>
                   <br></br>
+                  </div>
 
+                  <div class="row">
+                <div class="col-lg-6">
                   <label>
                   Time Frame (weeks):
                   </label>
-                  <input className="boxes" type="text" name="timeFrame" value={this.state.timeFrame} onChange={this.handleChange} />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className="" type="text" name="timeFrame" value={this.state.timeFrame} onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
 
+                  <div class="row">
+                <div class="col-lg-6">
                   <label>
                   Days Per Week (1-7):
                   </label>
-                  <input className="boxes" type="text" name="daysPerWeek" value={this.state.daysPerWeek}  onChange={this.handleChange} />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className="" type="text" name="daysPerWeek" value={this.state.daysPerWeek}  onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
 
+                  <div class="row">
+                <div class="col-lg-6">
                   <label>
                   Workout Length (minutes):
                   </label>
-                  <input className="boxes" type="text" name="workoutLength"  value={this.state.workoutLength} onChange={this.handleChange} />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className="" type="text" name="workoutLength"  value={this.state.workoutLength} onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
 
+                  <div class="row">
+                <div class="col-lg-6">
                   <label>
                   Experience Level:
                   </label>
-                  <input className="boxes" type="text" name="experienceLevel" value={this.state.experienceLevel} onChange={this.handleChange} />
+                  </div>
+                  <div class="col-lg-6">
+                  <input className="" type="text" name="experienceLevel" value={this.state.experienceLevel} onChange={this.handleChange} />
+                  </div>
                   <br></br>
+                  </div>
+                  </div>
 
                 </form>
+                </div>
                                 
                 </Container>
                 
@@ -169,10 +230,32 @@ class HomePage extends Component {
                 </h1>
       
                 <Container>
-      
-                <img className="image" src="https://media3.mensxp.com/media/content/2016/Oct/beginner-rsquo-s-training-guide-in-the-gym980-1476963053_1100x513.jpg" alt="beginner"></img>
-                <img className="image" src="https://media1.popsugar-assets.com/files/thumbor/jHfmIJteIm11bxFRxZLoXBFy3m4/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2018/12/28/846/n/1922729/02f8b6105c26771fc830f0.52460620_/i/Workout-Plan-Women.jpg" alt="intermediate"></img>
-                <img className="image" src="https://qph.fs.quoracdn.net/main-qimg-6f245bf4b6ac8c9069ac460183934ca8" alt="advanced"></img>
+                <div className="container">
+                <div className="row">
+                <div className="col-lg-6">      
+                <img className="image1" src="https://media3.mensxp.com/media/content/2016/Oct/beginner-rsquo-s-training-guide-in-the-gym980-1476963053_1100x513.jpg" alt="beginner" ></img>
+                </div>
+                <div className="col-lg-6">   
+                <h3>BEGINNER</h3>
+                </div>
+                </div>
+                <div className="row">
+                <div className="col-lg-6">
+                <h3>INTERMEDIATE</h3>
+                </div>
+                <div className="col-lg-6">
+                <img className="image2" src="https://media1.popsugar-assets.com/files/thumbor/jHfmIJteIm11bxFRxZLoXBFy3m4/fit-in/1024x1024/filters:format_auto-!!-:strip_icc-!!-/2018/12/28/846/n/1922729/02f8b6105c26771fc830f0.52460620_/i/Workout-Plan-Women.jpg" alt="intermediate"></img>
+                </div>
+                </div>
+                <div className="row">
+                <div className="col-lg-6">
+                <img className="image3" src="https://qph.fs.quoracdn.net/main-qimg-6f245bf4b6ac8c9069ac460183934ca8" alt="advanced"></img>
+                </div>
+                <div className="col-lg-6">
+                <h3>ADVANCED</h3>
+                </div>
+                </div>
+                </div>
       
                 </Container>
       
@@ -190,18 +273,21 @@ class HomePage extends Component {
                 </h1>
                 <br></br>
                   <div className="verify">
-                    <h2>First Name: {this.state.firstName} </h2> 
-                    <h2>Last Name: {this.state.lastName} </h2> 
-                    <h2>Email: {this.state.email} </h2> 
-                    <h2>Password: {this.state.password} </h2> 
-                    <h2>Your Weight: {this.state.weight} </h2> 
-                    <h2>Time Frame: {this.state.timeFrame} </h2> 
-                    <h2>Days Per Week: {this.state.daysPerWeek} </h2> 
-                    <h2>Workout Length : {this.state.workoutLength} </h2> 
-                    <h2>Experience Level: {this.state.experienceLevel} </h2> 
+                    <h4>First Name: {this.state.firstName} </h4> 
+                    <h4>Last Name: {this.state.lastName} </h4> 
+                    <h4>Email: {this.state.email} </h4> 
+                    <h4>Password: {this.state.password} </h4> 
+                    <h4>Your Weight: {this.state.weight} </h4> 
+                    <h4>Time Frame: {this.state.timeFrame} </h4> 
+                    <h4>Days Per Week: {this.state.daysPerWeek} </h4> 
+                    <h4>Workout Length : {this.state.workoutLength} </h4> 
+                    <h4>Experience Level: {this.state.experienceLevel} </h4> 
                   </div>
+                  <div class="row">
+                <div class="col-lg-12">
                   <input className="submit" type="submit" value="Submit" onClick={this.handleFormSubmit} />
-
+                </div>
+                </div>
                 
 
               </div>
