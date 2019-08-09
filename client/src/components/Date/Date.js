891c90e48fee69569c79de072cb658b0e67664fa
Ryan Doty <ryandoty@Ryans-MacBook-Pro.local>
2019-08-08 20:41:42 -0400

    wokring on organzing the logging of the sets in grid

---
+++ client/src/components/Date/Date.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Date extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default Date;
